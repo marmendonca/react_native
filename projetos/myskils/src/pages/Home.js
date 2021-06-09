9eb6758e128151fa3e149937012c3397abf62986
Marília Mendonça <marilianobre1@gmail.com>
2021-06-08 23:26:31 -0300

    up

---
+++ projetos/myskils/src/pages/Home.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TextInput, 
+    Platform,
+    TouchableOpacity
+} from 'react-native'
+
+export function Home(){
+  const [newSkill, setNewSkill] = useState();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Welcome, Marília
+      </Text>
+      
+      <TextInput style={styles.input}
+      placeholder="New Skill"
+      placeholderTextColor="#555"
+      onChangeText={setNewSkill}
+      />
+
+      <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText} activeOpacity={.7}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 50 }]}>
+          My Skills
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        backgroundColor:'#121015',
+        paddingHorizontal: 20,
+        paddingVertical: 70,
+        paddingHorizontal: 30
+    },
+    title: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    input: {
+        backgroundColor: '#1F1E25',
+        color: '#fff',
+        fontSize: 18,
+        padding: Platform.OS == 'ios' ? 15 : 10,
+        marginTop: 30,
+        borderRadius: 7
+    },
+    button: {
+        backgroundColor: '#A370F7',
+        padding: 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 20
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 17,
+        fontWeight: 'bold'
+    }
+})
