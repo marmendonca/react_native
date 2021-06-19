c29e72479a1210752b77910d7e0fe63345f45bdf
Marília Mendonça <marilianobre1@gmail.com>
2021-06-19 18:24:06 -0300

    Implementando componentes.
    Aprendendo a separar utilizações da minha aplicação com componentes, onde posso passar propriedades.

--- projetos/myskils/src/pages/Home.js
+++ projetos/myskils/src/pages/Home.js
@@ -7,6 +7,8 @@ import {
     Platform,
     TouchableOpacity
 } from 'react-native'
+import { Button } from '../components/Button';
+import { SkillCard } from '../components/SkillCard';
 
 export function Home(){
   const [newSkill, setNewSkill] = useState('');
@@ -28,20 +30,14 @@ export function Home(){
       onChangeText={setNewSkill}
       />
 
-      <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText} activeOpacity={.7} onPress={handleAddNewSkill}>Add</Text>
-      </TouchableOpacity>
+      <Button onPress={handleAddNewSkill}/>
 
       <Text style={[styles.title, { marginVertical: 50 }]}>
           My Skills
       </Text>
       {
           mySkills.map(skill => (
-            <TouchableOpacity key={skill} style={styles.buttonSkill}>
-                <Text style={styles.textSkill}>
-                        {skill}
-                </Text>
-            </TouchableOpacity>
+            <SkillCard skill={skill}/>
         ))
       }
     </View>
@@ -68,29 +64,5 @@ const styles = StyleSheet.create({
         padding: Platform.OS == 'ios' ? 15 : 10,
         marginTop: 30,
         borderRadius: 7
-    },
-    button: {
-        backgroundColor: '#A370F7',
-        padding: 15,
-        borderRadius: 7,
-        alignItems: 'center',
-        marginTop: 20
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 17,
-        fontWeight: 'bold'
-    },
-    buttonSkill: {
-        backgroundColor: '#1F1E25',
-        padding: 15,
-        borderRadius: 50,
-        alignItems: 'center',
-        marginVertical: 10
-    },
-    textSkill: {
-        color: '#FFF',
-        fontSize: 22,
-        fontWeight: 'bold',
     }
 })
